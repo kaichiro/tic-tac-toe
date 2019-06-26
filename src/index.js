4a4ba8dd2aebba0a7a2ab02623a83afee57a9de3
kaichiro <kcfg23@gmail.com>
2019-06-26 19:30:37 -0300

    Update files to deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker';
 
 import './index.css'
 
@@ -9,3 +10,5 @@ ReactDOM.render(
     <App>{console.log(`Build or ReBuild at ${new Date()}`)}</App>,
     document.getElementById('root')
 )
+
+serviceWorker.unregister();
